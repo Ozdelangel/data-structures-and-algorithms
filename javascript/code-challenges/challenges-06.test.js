8fc7b85c67ebbdac6c52416fc6e0de0c04b18542
OsbornDelAngel <ozdelangel@gmail.com>
2021-10-07 04:20:04 -0700

    start

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -37,6 +37,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
+  let newstr = str;
   
 };
 
