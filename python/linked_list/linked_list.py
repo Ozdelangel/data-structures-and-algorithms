1059d8092f3576da2aa54bd19350aea9766ce4cf
OsbornDelAngel <ozdelangel@gmail.com>
2021-12-18 17:46:12 -0800

    linked list implementation

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,30 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
-
-    def __init__(self):
-        # initialization here
+    def __init__(self,head=None):
+        self.head = head
+        # ll head of 1
+        # (1) -> (2) -> none
+        # insert 3
+        # (3) -> (1) -> (2) -> None
+    def insert(self, value):
+        node = Node(value) # will create a node of 3
+        if self.head is not None:
+           node.next = self.head
+        # we say 3's next is the current head which is 1
+        # we have to tell the link list that we have a new head
+        self.head = node
+    def includes(self, x):
+        current = self.head
+        while current is not None:
+            if current.value == x:
+                return True
+            current = current.next
+        return False
+    def to_string(self):
         pass
 
-    def some_method(self):
-        # method body here
-        pass
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,43 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
+def test_node_instance():
+    node = Node(1, None)
+    assert node.next == None
+    assert node.value == 1
 
+def test_node_instance_fail():
+    node = Node(1, None)
+    assert node.next != node
+    assert node.value != 2
+def test_linked_list():
+    node = Node(1, None)
+    ll = LinkedList(node)
+    assert ll.head == node
+
+def linked_list_empty():
+    ll = LinkedList()
+    assert ll.head == None
+
+def test_linked_list_insert_to_empty():
+    #  ll.head = apple
+    ll = LinkedList()
+    ll.insert('apple')
+    assert ll.head.value == 'apple'
+
+def test_insert_to_linked_list():
+    # ll
+    # node 1
+    # node2
+    # ll.head = apple
+    # apple.next = pear
+    # pear.next = none
+    # apple -> pear -> none
+    ll = LinkedList()
+    node1 = Node('apple')
+    ll.head == node1
+    node2 = Node('pear')
+    node1.next = node2
+    ll.insert('bananna')
+    assert ll.head.value == 'bananna'
 def test_import():
     assert LinkedList
